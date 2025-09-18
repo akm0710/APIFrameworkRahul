0ffc0e96035ff9f5544c84be6d55d7d4f37d5cda
ashish <ashishmishra.0710@gmail.com>
2025-09-18 11:07:47 +0530

    html report

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -7,6 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/java/features",
+		plugin="json:target/jsonReports/cucumber-report.json",
 		glue= {"stepDefinations"})
 public class TestRunner {
 //tags= {"@DeletePlace"}  compile test verify
